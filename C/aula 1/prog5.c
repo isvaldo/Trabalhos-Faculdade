4df3b8cac61dcbe100328f029618ed0de53f723a
isvaldo <miojex360@hotmail.com>
2014-10-05 01:08:50 -0300

    exercice 5 6 7

--- C/aula 1/prog5.c
+++ C/aula 1/prog5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /***********************************************************
-*faça um algoritmo que calcule o novo valor de um salário a *
+*faça um algoritmo que calcule o novo valor de um salário a*
 * partir de um valor percentual de reajuste. O valor atual *
 *do salário e o valor percentual do reajuste devem ser     *
 * informados pelo usuário como, por exemplo, 7,3%.         *
---
+++ C/aula 1/prog7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**************************************************************
+*7. Faça um algoritmo que calcule a área de um retângulo,     *
+* sendo que os comprimentos da altura e da base são informados*
+* pelo usuário. A área do retângulo é calculada               *
+***************************************************************/
+float base;
+float altura;
+float area;
+
+main () {
+printf("\n informe a base: ");
+scanf("%f",&base);
+printf("\n informe a altura: ");
+scanf("%f",&altura);
+
+area = base*altura;
+printf("\n area e %.2f\n",area);
+
+
+}
