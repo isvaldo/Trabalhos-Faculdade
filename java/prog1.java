999846aec485c8bb3f936d4f613e15a73f1b2c04
isvaldo <miojex360@hotmail.com>
2014-10-11 17:29:55 -0300

    Remove trash

---
+++ java/prog1.java
@@ -0,0 +1,28 @@
+/*
+ O usuário digita com um número que representa um mês do ano, e o 
+programa imprime na tela um evento importante que aconteça neste mês. 
+(If - Else) 
+ */
+
+import java.util.Scanner;
+
+public class prog1 {
+	
+	public static void main (String args[]) {
+		
+		//declaração de variaveis
+		int mes;
+		// Instancia leia de Scanner, essa variavel vai monitorar
+		// System.in (Entrada de dados pelo teclado)
+		Scanner leia = new Scanner(System.in);
+		System.out.print("Informe um numero correspondente a um mes: ");
+		// mes recebe valor inteiro informado no terminal
+		mes = leia.nextInt();
+		
+		
+		if (mes == 2) {
+		}
+		
+	}
+}
+
