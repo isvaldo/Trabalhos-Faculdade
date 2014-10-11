5004e11fab4f2b200b8eb0309adff6b23c526731
isvaldo <miojex360@hotmail.com>
2014-10-11 20:11:35 -0300

    Update gitignore and make exercice fixed #4

--- java/Aula 2/prog1.java
+++ java/Aula 2/prog1.java
@@ -19,10 +19,45 @@ public class prog1 {
 		// mes recebe valor inteiro informado no terminal
 		mes = leia.nextInt();
 		
-		
-		if (mes == 2) {
-		}
-		
+		if (mes == 1) {
+			System.out.print("Janeiro:\n");
+			System.out.print("Confraternização Universal Dia Mundial da Paz");
+		}else if (mes==2) {
+			System.out.print("Fevereiro:\n");
+			System.out.print("Dia Internacional da Mulher");
+			}else if (mes==3) {
+				System.out.print("Março:\n");
+				System.out.print("Carnaval");
+				}else if (mes==4) {
+					System.out.print("Abril:\n");
+					System.out.print("Dia da Mentira");
+					}else if (mes==5) {
+						System.out.print("Maio:\n");
+						System.out.print("Dia das Mães");
+						}else if (mes==6) {
+							System.out.print("Junho:\n");
+							System.out.print("Dia de São João");
+							}else if (mes==7) {
+								System.out.print("Julho:\n");
+								System.out.print("Dia da Liberdade de Pensamento");
+								}else if (mes==8) {
+									System.out.print("Agosto:\n");
+									System.out.print("Dia dos Pais");
+									}else if (mes==9) {
+										System.out.print("Setembro:\n");
+										System.out.print("Independência do Brasil");
+										}else if (mes==10) {
+											System.out.print("Outubro:\n");
+											System.out.print("Dia das Bruxas - Halloween ");
+											}else if (mes==11) {
+												System.out.print("Novembro:\n");
+												System.out.print("Proclamação da República");
+												}else if (mes==12) {
+													System.out.print("Dezembro:\n");
+													System.out.print("Reveillon");
+													}else {
+														System.out.print("Mes invalido");
+														}
 	}
 }
 
