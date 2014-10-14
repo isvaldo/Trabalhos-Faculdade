2fdb4cf5526809bb09685b5b394d48066fa6ec65
Isvaldo Fernandes de souza <isvaldo.fernandes@gmail.com>
2014-10-14 11:41:23 -0300

    Create prog1.c

---
+++ C/Aula-03/prog1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+*Faça um algoritmo que leia uma sequência de caracteres
+*terminada por um ponto e mostre o numero de vogais da frase.
+*/
+
+main () {
+char sequencia[100];
+
+while(1) {
+    
+}
+
+}
