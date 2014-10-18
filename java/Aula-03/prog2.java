34339668b3249543f23424c259f7b14699be508f
isvaldo <miojex360@hotmail.com>
2014-10-18 20:35:30 -0300

    Exercicio 1 e 2 feitos java. solved #5

--- java/Aula-03/prog2.java
+++ java/Aula-03/prog2.java
@@ -1,15 +1,20 @@
 /*
-* Declarar um vetor de 20 posições com valores entre 1 e 1000. Percorrer o 
+* Declarar um vetor de 20 posições ALEATORIAS com valores entre 1 e 1000. Percorrer o 
 * vetor indicando, para um determinado elemento, se ele é múltiplo de 2, 3,
 * 5 ou 7.
  */
 
-public class prog1 {
+import java.util.Random;
+public class prog2 {
 	
 public static void main(String[] args) {
 	
-	int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int[] numeros = new int[20];
+	Random rand = new Random();
 	
+	for(int i=0;i<20;i++) 
+		numeros[i] = rand.nextInt(999)+1; // no caso 0, ele soma + 1 e vira 1, no caso 999 soma + 1 e vira 1000
+		
 	for (int item:numeros) {
 		if (item % 2 == 0)
 		System.out.printf("%d e divisivel por 2 \n",item);
