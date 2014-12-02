0bd6de08c18736a32716edadca34a120af1470da
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2014-12-02 14:56:33 -0200

    Create lerString.c

---
+++ C/av3/lerString.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+// vetor é um ponteiro.
+void ler(char vetorString[5][255]){
+    char character[255];
+    int i;
+    for(i=0;i<5;i++) {
+            scanf(" %s",&character);
+            strcpy(vetorString[i],character);
+            fflush(stdin);
+        }
+}
+
+
+
+main () {
+    char nomes[5][255];
+    ler(nomes);
+    puts("Hora da verdade");
+    int i;
+    
+
+
+}
