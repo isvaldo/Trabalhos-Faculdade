7230a5ac3a630e26b290ceb63323c25b3c1a8d59
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 16:16:01 -0200

    Create reverse.java

---
+++ java/OCA/StringBuilder/reverse.java
@@ -0,0 +1,21 @@
+package a.b.c;
+
+
+public class D {
+	public static void main(String[] args) {
+	StringBuilder texto = new StringBuilder("ola mundo");
+	String novo = texto.reverse().toString();
+	System.out.println(novo);
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
