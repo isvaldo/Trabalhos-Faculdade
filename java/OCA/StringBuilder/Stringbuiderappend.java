188fb3486914e9074c39d9aa2eaa7661b9453d62
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 16:03:06 -0200

    Create Stringbuiderappend.java

---
+++ java/OCA/StringBuilder/Stringbuiderappend.java
@@ -0,0 +1,8 @@
+public class D {
+	public static void main(String[] args) {
+	StringBuilder texto = new StringBuilder("ola").append(" mundo").append(new StringBuilder(" ola"));
+	System.out.println(texto);
+	}
+
+	
+}
