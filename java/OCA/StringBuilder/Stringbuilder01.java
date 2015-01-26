28bac66b9235aecccd4311aa1fc3dfced55fe94c
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 15:44:37 -0200

    Create Stringbuilder01.java

---
+++ java/OCA/StringBuilder/Stringbuilder01.java
@@ -0,0 +1,9 @@
+public class D {
+	public static void main(String[] args) {
+	//  mesmo que String texto  =  "ola" + "mundo";
+	StringBuilder texto = new StringBuilder("ola").append(" mundo");
+	System.out.println(texto);
+	}
+
+	
+}
