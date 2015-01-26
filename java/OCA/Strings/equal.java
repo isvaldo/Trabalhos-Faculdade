7cebd51c914b4d4a31f6486b0d7a823f39b0291a
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 17:09:27 -0200

    Create equal.java

---
+++ java/OCA/Strings/equal.java
@@ -0,0 +1,26 @@
+package a.b.c;
+
+public class D {
+	public static void main(String[] args) {
+		
+		String c = "ola";
+		String c2 = "OLA";
+		
+		
+		System.out.println((c.equals(c2)));
+		// ambos são upper
+		System.out.println((c.toUpperCase().equals(c2.toUpperCase())));
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
