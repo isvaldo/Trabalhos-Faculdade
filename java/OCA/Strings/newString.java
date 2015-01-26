a7bceff96b9b3afe07f314ad38adcd303303f225
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 16:46:03 -0200

    Create newString.java

---
+++ java/OCA/Strings/newString.java
@@ -0,0 +1,19 @@
+  package a.b.c;
+
+
+public class D {
+	public static void main(String[] args) {
+		
+		char[] c = new char[]{'a','b','c','d'};
+		StringBuilder bs = new StringBuilder("Java");
+		StringBuffer bfs = new StringBuffer("java");
+		
+		
+		
+		System.out.println(new String(c));
+		System.out.println(new String(bs));
+		System.out.println(new String(bfs));
+		
+	}
+
+	
