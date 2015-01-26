b8bfb8211eb673f032bc1e9c2555f7df2580008a
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 17:11:04 -0200

    Create equalsIgnoreCase.java

---
+++ java/OCA/Strings/equalsIgnoreCase.java
@@ -0,0 +1,25 @@
+package a.b.c;
+
+public class D {
+	public static void main(String[] args) {
+		
+		String c = "ola";
+		String c2 = "OLA";
+		
+		// ignora o Case !!
+		System.out.println((c.equalsIgnoreCase(c2)));
+
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
