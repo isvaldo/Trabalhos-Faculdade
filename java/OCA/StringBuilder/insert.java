c4bd6649486288ee96b6472c64110ad96517ecdb
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 16:07:20 -0200

    Create insert.java

---
+++ java/OCA/StringBuilder/insert.java
@@ -0,0 +1,11 @@
+package a.b.c;
+
+
+public class D {
+	public static void main(String[] args) {
+	StringBuilder texto = new StringBuilder("ola").insert(0,"1");
+	System.out.println(texto);
+	}
+
+	
+}
