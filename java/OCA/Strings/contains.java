9ed277175c4256710bbedb7d28e2602b22f7c6f2
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-26 17:23:13 -0200

    Create contains.java

---
+++ java/OCA/Strings/contains.java
@@ -0,0 +1,28 @@
+package a.b.c;
+
+public class D {
+	public static void main(String[] args) {
+		
+		String c = "link é um jogo muito antigo";
+		
+		String c2 = "linkéumjogomuito antigo";
+		// ignora o Case !!
+		System.out.println((c.contains("jogo")));
+		//true
+		System.out.println((c2.contains("jogo")));
+		//true
+
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
