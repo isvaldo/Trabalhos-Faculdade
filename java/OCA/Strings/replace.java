00e888d77e3be97e232bd154083c9259a0c69b77
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-27 15:35:27 -0200

    Create replace.java

---
+++ java/OCA/Strings/replace.java
@@ -0,0 +1,20 @@
+package a.b.c;
+
+ public class D {
+	 public static void main(String[] args) {
+		 String a = "ola mundo";
+		 a.replace("a", "o");
+		 a.replace('a', "ola");
+		 // a.replace("a",'o'); // erro de assinatura  
+		 System.out.println(a);
+	}
+ }
+
+
+
+
+
+
+
+
+
