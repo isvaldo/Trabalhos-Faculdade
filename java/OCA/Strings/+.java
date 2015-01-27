526e813588ac9c2ddd3e3b146a5b3a87ec253e1a
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-27 15:26:58 -0200

    Create +.java

---
+++ java/OCA/Strings/+.java
@@ -0,0 +1,22 @@
+package a.b.c;
+
+ public class D {
+	 public static void main(String[] args) {
+		int valor = 10;
+		int dividePor = 4;
+		double resultado = valor / dividePor; // double <- int (2)
+		System.out.println(valor + dividePor + 
+				" São valores");
+		
+		System.out.println(resultado + " e o resultado");
+	}
+ }
+
+
+
+
+
+
+
+
+
