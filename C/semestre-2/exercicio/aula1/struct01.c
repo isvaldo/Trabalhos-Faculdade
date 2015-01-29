d3d61daa176710fdb2476df0b08d921625da5641
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-01-29 16:44:19 -0200

    Update struct01.c

--- C/semestre-2/exercicio/aula1/struct01.c
+++ C/semestre-2/exercicio/aula1/struct01.c
@@ -8,14 +8,16 @@ struct func {
     int identidade;
     char nascimento[T_STRING];
     };
-// pega as entradas de caracteres
 
 main() {
+    // criar o registro
     struct func f;
+    
+    // seta os valores
     strcpy(f.nome,"isvaldo");
     f.identidade = 154;
     strcpy(f.nascimento,"05/15/1993");
-
+    // mostra os na tela
     printf("\nNome:%s\n",f.nome);
     printf("identidade:%d\n",f.identidade);
     printf("nascimento:%s\n",f.nascimento);
