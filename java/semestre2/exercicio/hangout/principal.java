2880f5f054608a36e916165938ce747354f14010
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-04-05 19:19:33 -0300

    Create principal.java

---
+++ java/semestre2/exercicio/hangout/principal.java
@@ -0,0 +1,19 @@
+package av2.palavra;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		
+	
+		FraseAleatoria lero = new FraseAleatoria(100);
+		
+		
+		
+		System.out.println(lero.toString());
+		
+		
+		
+	}
+
+}
