277237cb3842b6bf37cc2d1ebebb857b7525e630
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-04-07 12:05:54 -0300

    Create Forma.java

---
+++ java/semestre2/exercicio/av3/Forma.java
@@ -0,0 +1,11 @@
+package prova.biscoito;
+
+/*Constantes para a forma do biscoito
+ */
+public enum Forma {
+	A("A"), B("B"), C("C"), D("D");
+	private final String s;
+	
+	// construtor
+	Forma(String s) { this.s = s; }
+}
