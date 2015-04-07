7a8ed991c8627e3424e21e04a5ee5c301001ec12
Isvaldo Fernandes de Souza <isvaldo.fernandes@gmail.com>
2015-04-07 12:03:38 -0300

    Create Tipo.java

---
+++ java/semestre2/exercicio/av3/Tipo.java
@@ -0,0 +1,12 @@
+package prova.biscoito;
+
+/*Constantes para o tipo do biscoito
+ * */
+public enum Tipo {
+	DOCE("D"),SALGADO("S");
+	private final String s;
+	
+	// construtor
+	Tipo(String s) { this.s = s; }
+	
+}
